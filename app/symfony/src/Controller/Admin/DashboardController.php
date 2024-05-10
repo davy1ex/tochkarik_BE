dd2847455e6890b8f2d6aa77fabb4c3de38bf7fd
davy1ex <davy1ex@yandex.ru>
2024-05-11 00:37:32 +0400

    Backup master

--- app/symfony/src/Controller/Admin/DashboardController.php
+++ app/symfony/src/Controller/Admin/DashboardController.php
@@ -38,64 +38,10 @@ class DashboardController extends AbstractDashboardController
 
     public function configureDashboard(): Dashboard
     {
-//        return Dashboard::new()
-//            ->setTitle('Symfony');
         return Dashboard::new()
-            // the name visible to end users
-            ->setTitle('ACME Corp.')
-            // you can include HTML contents too (e.g. to link to an image)
-            ->setTitle('<img src="..."> ACME <span class="text-small">Corp.</span>')
+            ->setTitle('Symfony');
 
-            // by default EasyAdmin displays a black square as its default favicon;
-            // use this method to display a custom favicon: the given path is passed
-            // "as is" to the Twig asset() function:
-            // <link rel="shortcut icon" href="{{ asset('...') }}">
-            ->setFaviconPath('favicon.svg')
 
-            // the domain used by default is 'messages'
-            ->setTranslationDomain('my-custom-domain')
-
-            // there's no need to define the "text direction" explicitly because
-            // its default value is inferred dynamically from the user locale
-            ->setTextDirection('ltr')
-
-            // set this option if you prefer the page content to span the entire
-            // browser width, instead of the default design which sets a max width
-            ->renderContentMaximized()
-
-            // set this option if you prefer the sidebar (which contains the main menu)
-            // to be displayed as a narrow column instead of the default expanded design
-            ->renderSidebarMinimized()
-
-            // by default, users can select between a "light" and "dark" mode for the
-            // backend interface. Call this method if you prefer to disable the "dark"
-            // mode for any reason (e.g. if your interface customizations are not ready for it)
-            ->disableDarkMode()
-
-            // by default, all backend URLs are generated as absolute URLs. If you
-            // need to generate relative URLs instead, call this method
-            ->generateRelativeUrls()
-
-            // set this option if you want to enable locale switching in dashboard.
-            // IMPORTANT: this feature won't work unless you add the {_locale}
-            // parameter in the admin dashboard URL (e.g. '/admin/{_locale}').
-            // the name of each locale will be rendered in that locale
-            // (in the following example you'll see: "English", "Polski")
-            ->setLocales(['en', 'pl'])
-            // to customize the labels of locales, pass a key => value array
-            // (e.g. to display flags; although it's not a recommended practice,
-            // because many languages/locales are not associated to a single country)
-            ->setLocales([
-                'en' => '🇬🇧 English',
-                'pl' => '🇵🇱 Polski'
-            ])
-            // to further customize the locale option, pass an instance of
-            // EasyCorp\Bundle\EasyAdminBundle\Config\Locale
-            ->setLocales([
-                'en', // locale without custom options
-//                Locale::new('pl', 'polski', 'far fa-language') // custom label and icon
-            ])
-            ;
     }
 
     public function configureMenuItems(): iterable
