62fcab56dce88f9627fa861563cba10808e56424
davy1ex <dezy1ex@gmail.com>
2024-05-20 14:38:46 +0400

    MS_2-T_1 - Added Easy admin

--- backend/migrations/Version20240520062434.php
+++ backend/migrations/Version20240520062434.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240519112729 extends AbstractMigration
+final class Version20240520062434 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,6 @@ final class Version20240519112729 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE "user_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE "user" (id INT NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, roles JSON DEFAULT NULL, username VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, profile_picture VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
     }
 
---
+++ backend/src/Controller/UserController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class UserController extends AbstractController
+{
+//    #[Route('/user', name: 'app_user')]
+//    public function index(): Response
+//    {
+//        return $this->render('user/index.html.twig', [
+//            'controller_name' => 'UserController',
+//        ]);
+//    }
+
+    /**
+     * @Route("/api/user/{id}", name="api_user_show", methods={"GET"})
+     */
+    #[Route('/api/user/{id}', name: 'app_user_show', methods: ['GET'])]
+    public function show(UserRepository $userRepository, $id): JsonResponse
+    {
+        $user = $userRepository->find($id);
+
+        if (!$user) {
+            return $this->json(['message' => 'User not found'], 404);
+        }
+
+        return $this->json([
+            'username' => $user->getUsername(),
+            'email' => $user->getEmail(),
+            // Add other fields as needed
+        ]);
+    }
+}
+
+
