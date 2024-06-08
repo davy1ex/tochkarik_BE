007bd8510f7f3eae26746869135480f299c6721c
davy1ex <dezy1ex@gmail.com>
2024-06-08 15:25:13 +0300

    MS3_T3 - User - Fix user_get api

--- backend/src/Controller/UserController.php
+++ backend/src/Controller/UserController.php
@@ -63,6 +63,40 @@ class UserController extends AbstractController
             return $this->json(['error' => 'Internal Server Error'], JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
+
+    #[Route('/api/user/get_user', name: 'app_user_show', methods: ['GET'])]
+    public function get_user(Request $request, UserRepository $userRepository): JsonResponse
+    {
+        try {
+            $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+            $user_id = $request->query->get('user_id');
+            if (!$user_id || !is_numeric($user_id)) {
+                return $this->json(['error' => 'Invalid user_id'], JsonResponse::HTTP_BAD_REQUEST);
+            }
+
+            $user_id = (int) $user_id;
+            $currentUser = $this->security->getUser();
+            if ($currentUser->getId() !== $user_id) {
+                return $this->json(['error' => 'Forbidden'], JsonResponse::HTTP_FORBIDDEN);
+            }
+
+            $user = $userRepository->find($user_id);
+            if (!$user) {
+                return $this->json(['error' => 'User not found'], JsonResponse::HTTP_NOT_FOUND);
+            }
+
+            return $this->json([
+                'username' => $user->getUsername(),
+            ]);
+
+        } catch (\Exception $e) {
+//            $this->get('logger')->error('Error fetching points: ' . $e->getMessage());
+            return $this->json(['error' => $e->getMessage()], JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
 }
 
 
