b7f647358f42cccead5e7291e673f3f60c64be6d
davy1ex <dezy1ex@gmail.com>
2024-06-08 15:33:45 +0300

    MS3_T2 - MapComponent.tsx - Add see street of generatedPoint

--- frontend/tochkarik/src/components/Map/MapComponent.tsx
+++ frontend/tochkarik/src/components/Map/MapComponent.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Circle, Marker, useMap } from 'react-leaflet';
 
+import axios from 'axios';
+
 import "leaflet/dist/leaflet.css";
 
 import "./Map.css";
@@ -17,12 +19,18 @@ import BigBtn from '../buttons/Button';
 const MapComponent: React.FC = () => {
     const [position, setPosition] = useState<[number, number]>([53.242, 50.221]);
     const [radius, setRadius] = useState<number>(1000);
+
+    const [street, setStreet] = useState<string>()
+
     const [markerPosition, setMarkerPosition] = useState<[number, number] | null>(null);
     const [showCircle, setShowCircle] = useState<boolean>(true);
+
     const [showSlider, setShowSlider] = useState<boolean>(true);
     const [showGenerateBtn, setShowGenerateBtn] = useState<boolean>(true);
     const [showGenerateNewBtn, setShowGenerateNewBtn] = useState<boolean>(false);
+
     const [error, setError] = useState<string>('');
+
 
     const geoLocation = useGeoLocation(setError);
     const generateRandomCoordinates = useRandomCoordinates();
@@ -41,6 +49,13 @@ const MapComponent: React.FC = () => {
         if (position) {
             const [newLatitude, newLongitude] = generateRandomCoordinates(position, radius);
             setMarkerPosition([newLatitude, newLongitude]);
+
+            axios.get(`https://nominatim.openstreetmap.org/search?accept-language=ru&format=json&q=${newLatitude} ${newLongitude}`)
+                .then(response => {
+                    console.log(response),
+                    setStreet(response.data[0].display_name)
+                })
+
             setShowCircle(false);
             setShowGenerateBtn(false);
             setShowGenerateNewBtn(true);
@@ -83,9 +98,10 @@ const MapComponent: React.FC = () => {
             </MapContainer>
 
             <div className="controls-container">
+                {street}
                 {showSlider && <RadiusSlider radius={radius} handleRadiusChange={handleRadiusChange} />}
                 {showGenerateBtn && <BigBtn onClick={handleGenerate}>Generate</BigBtn>}
-                {showGenerateNewBtn && <BigBtn onClick={handleGenerateNew}>Generate new</BigBtn>}
+                {showGenerateNewBtn && <BigBtn onClick={handleGenerateNew}>Cancel Journey</BigBtn>}
                 {error && <ErrorMessage message={error} />}
                 <ManualLocationInput setPosition={setPosition} setError={setError} />
             </div>
