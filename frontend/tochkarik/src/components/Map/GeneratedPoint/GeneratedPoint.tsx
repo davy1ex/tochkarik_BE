8be3057f20711476a6a22bdcdda85f47e85302a2
davy1ex <dezy1ex@gmail.com>
2024-06-11 12:47:53 +0300

    MS3_T2 - many file of Front for adding features - Add adding point to frontend

---
+++ frontend/tochkarik/src/components/Map/GeneratedPoint/GeneratedPoint.tsx
@@ -0,0 +1,94 @@
+import React, {useState} from 'react';
+
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import BigBtn from '../../buttons/Button';
+import AddPoint from '../AddPoint/AddPoint'
+
+import bookmark from '../../../icons/bookmark.svg'
+import bookmarked from '../../../icons/bookmarked.svg'
+import "./GeneratedPoint.css";
+
+
+
+interface GeneratedPointProps {
+    street: string;
+    pointTitle: string;
+    isNew: boolean;
+    hasReport: boolean;
+    onCancel: () => void;
+    onStartJourney: () => void;
+    onCreateReport: () => void;
+    onEditReport: () => void;
+
+    name: string;
+    coordinates: [number, number];
+    timeOfGenerate: string;
+}
+
+const GeneratedPoint: React.FC<GeneratedPointProps> = ({
+    street,
+    pointTitle,
+    isNew,
+    hasReport,
+    onCancel,
+    onStartJourney,
+    onCreateReport,
+    onEditReport,
+
+    coordinates,
+    timeOfGenerate,
+
+}) => {
+    const [isBookmarked, setIsBookmarked] = useState<boolean>(false)
+    const [showAddPointDialog, setShowAddPointDialog] = useState<boolean>(false);
+
+    const handleAddBookmark = () => {
+        setShowAddPointDialog(true);
+        setIsBookmarked(true);
+
+    };
+
+    const addButtonCancelHandler = () => {
+        setShowAddPointDialog(false);
+    };
+
+    return (
+        <div className="controls-container-generated">
+            <div className={"controls-wrapper-generated"}>
+                <div className={"controls-header-container"}>
+                    <h1 className={"point-title"}>{pointTitle}</h1>
+
+                    {isBookmarked ? (
+                        <div> <img src={bookmarked} alt={"Bookmarked"} /></div>
+                    ):(
+                        <div onClick={() => handleAddBookmark(name, timeOfGenerate, coordinates)}><img src={bookmark} alt={"Bookmark"}/></div>
+                    )}
+                </div>
+
+                <p>{street}</p>
+                <div className="button-group">
+                    <BigBtn onClick={onStartJourney}>Start Journey</BigBtn>
+                    {hasReport ? (
+                        <BigBtn onClick={onEditReport}>Edit Report</BigBtn>
+                    ) : (
+                        <BigBtn onClick={onCreateReport}>Create Report</BigBtn>
+                    )}
+                    {isNew && (
+                        <BigBtn onClick={onCancel}>Cancel</BigBtn>
+                    )}
+                </div>
+                {hasReport && <ErrorMessage message="Report exists" />}
+            </div>
+
+            {showAddPointDialog && (
+                <AddPoint
+                    addButtonCancelHandler={addButtonCancelHandler}
+                    position={coordinates}
+                    timeOfGenerate={timeOfGenerate}
+                />
+            )}
+        </div>
+    );
+};
+
+export default GeneratedPoint;
--- frontend/tochkarik/src/components/Map/MapComponent.tsx
+++ frontend/tochkarik/src/components/Map/MapComponent.tsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Circle, Marker, useMap } from 'react-leaflet';
-
 import axios from 'axios';
 
 import "leaflet/dist/leaflet.css";
 
-import "./Map.css";
+import moment from 'moment-timezone';
+import tzlookup from 'tz-lookup';
 
 import RadiusSlider from './Slider/RadiusSlider';
 import ManualLocationInput from './ManualLocation/ManualLocationInput';
@@ -13,24 +13,19 @@ import ErrorMessage from './ErrorMessage/ErrorMessage';
 
 import useGeoLocation from './hooks/useGeoLocation';
 import useRandomCoordinates from './hooks/useRandomCoordinates';
-
+import GeneratedPoint from './GeneratedPoint/GeneratedPoint';
 import BigBtn from '../buttons/Button';
+import "./Map.css";
+
 
 const MapComponent: React.FC = () => {
     const [position, setPosition] = useState<[number, number]>([53.242, 50.221]);
     const [radius, setRadius] = useState<number>(1000);
-
-    const [street, setStreet] = useState<string>()
-
+    const [street, setStreet] = useState<string>('');
     const [markerPosition, setMarkerPosition] = useState<[number, number] | null>(null);
-    const [showCircle, setShowCircle] = useState<boolean>(true);
-
-    const [showSlider, setShowSlider] = useState<boolean>(true);
-    const [showGenerateBtn, setShowGenerateBtn] = useState<boolean>(true);
-    const [showGenerateNewBtn, setShowGenerateNewBtn] = useState<boolean>(false);
-
+    const [showControls, setShowControls] = useState<boolean>(true);
+    const [timeOfGenerate, setTimeOfGenerate] = useState<string | null>(null);
     const [error, setError] = useState<string>('');
-
 
     const geoLocation = useGeoLocation(setError);
     const generateRandomCoordinates = useRandomCoordinates();
@@ -45,32 +40,44 @@ const MapComponent: React.FC = () => {
         setRadius(Number(event.target.value));
     };
 
-    const handleGenerate = () => {
+    const handleGenerate = async () => {
         if (position) {
             const [newLatitude, newLongitude] = generateRandomCoordinates(position, radius);
             setMarkerPosition([newLatitude, newLongitude]);
 
             axios.get(`https://nominatim.openstreetmap.org/search?accept-language=ru&format=json&q=${newLatitude} ${newLongitude}`)
                 .then(response => {
-                    console.log(response),
-                    setStreet(response.data[0].display_name)
-                })
+                    setStreet(response.data[0].display_name);
+                });
 
-            setShowCircle(false);
-            setShowGenerateBtn(false);
-            setShowGenerateNewBtn(true);
-            setShowSlider(false);
+            setShowControls(false);
+
+            const timeZone = tzlookup(newLatitude, newLongitude);
+            const zonedTime = moment.tz(new Date(), timeZone);
+            const formattedTime = zonedTime.format('YYYY-MM-DD HH:mm:ss');
+            setTimeOfGenerate(formattedTime);
         } else {
             setError('Position not determined. Please enter your location manually.');
         }
     };
 
-    const handleGenerateNew = () => {
+    const handleCancel = () => {
         setMarkerPosition(null);
-        setShowCircle(true);
-        setShowGenerateBtn(true);
-        setShowGenerateNewBtn(false);
-        setShowSlider(true);
+        setShowControls(true);
+        setStreet('');
+        setTimeOfGenerate(null);
+    };
+
+    const handleStartJourney = () => {
+        // Implement start journey logic here
+    };
+
+    const handleCreateReport = () => {
+        // Implement create report logic here
+    };
+
+    const handleEditReport = () => {
+        // Implement edit report logic here
     };
 
     const UpdateMapPosition: React.FC<{ position: [number, number] }> = ({ position }) => {
@@ -86,25 +93,45 @@ const MapComponent: React.FC = () => {
     };
 
     return (
-        <div className="map-container">
-            <MapContainer center={position} zoom={13}>
-                <TileLayer
-                    url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                />
-                {position && <UpdateMapPosition position={position} />}
-                {showCircle && position && <Circle center={position} radius={radius} />}
-                {markerPosition && <Marker position={markerPosition} />}
-            </MapContainer>
-
-            <div className="controls-container">
-                {street}
-                {showSlider && <RadiusSlider radius={radius} handleRadiusChange={handleRadiusChange} />}
-                {showGenerateBtn && <BigBtn onClick={handleGenerate}>Generate</BigBtn>}
-                {showGenerateNewBtn && <BigBtn onClick={handleGenerateNew}>Cancel Journey</BigBtn>}
-                {error && <ErrorMessage message={error} />}
-                <ManualLocationInput setPosition={setPosition} setError={setError} />
+        <div className="map-component">
+            <div className="map-container">
+                <MapContainer center={position} zoom={13}>
+                    <TileLayer
+                        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    />
+                    {position && <UpdateMapPosition position={position} />}
+                    {showControls && position && <Circle center={position} radius={radius} />}
+                    {markerPosition && <Marker position={markerPosition} />}
+                </MapContainer>
             </div>
+
+            {showControls ? (
+                <div className="controls-container">
+                    <div className={"controls-container-wrapper"}>
+                        {street}
+                        <RadiusSlider radius={radius} handleRadiusChange={handleRadiusChange} />
+                        <BigBtn onClick={handleGenerate}>Generate</BigBtn>
+                        <ManualLocationInput setPosition={setPosition} setError={setError} />
+                        {error && <ErrorMessage message={error} />}
+                    </div>
+
+                </div>
+            ) : (
+                <GeneratedPoint
+                    street={street}
+                    pointTitle="Point Generated"
+                    isNew={true}
+                    hasReport={false}  // Change this based on your logic
+                    onCancel={handleCancel}
+                    onStartJourney={handleStartJourney}
+                    onCreateReport={handleCreateReport}
+                    onEditReport={handleEditReport}
+
+                    coordinates={position}
+                    timeOfGenerate={timeOfGenerate}
+                />
+            )}
         </div>
     );
 };
