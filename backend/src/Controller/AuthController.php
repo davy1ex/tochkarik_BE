ebae170519db290d838b31aec811fc28dc3224c3
davy1ex <dezy1ex@gmail.com>
2024-06-14 13:07:41 +0300

    AuthController.php - Refactor file + add point to check correct api req from Frontend

--- backend/src/Controller/AuthController.php
+++ backend/src/Controller/AuthController.php
@@ -2,25 +2,15 @@
 
 namespace App\Controller;
 
-use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 class AuthController extends AbstractController
 {
-    #[Route('/api/auth/signin', name: 'api_auth_signin', methods: ['POST'])]
-    public function signin(UserInterface $user, JWTTokenManagerInterface $jwtManager): JsonResponse
+    #[Route('/api/user/check_token', name: 'api_check_token', methods: ['GET'])]
+    public function checkToken()
     {
-        $token = $jwtManager->create($user);
-
-        return new JsonResponse([
-            'token' => $token,
-            'user_data' => [
-                'user_id' => $user->getId(),
-                'username' => $user->getUsername(),
-            ],
-        ]);
+        return new JsonResponse(['status' => 'Token is valid']);
     }
 }
