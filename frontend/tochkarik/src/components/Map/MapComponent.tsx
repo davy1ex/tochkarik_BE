564114173fe236895021de84cff34c458039dd3d
davy1ex <dezy1ex@gmail.com>
2024-06-19 12:20:46 +0300

    MS4 - MapComponent.tsx - Fix ungettable img of points

--- frontend/tochkarik/src/components/Map/MapComponent.tsx
+++ frontend/tochkarik/src/components/Map/MapComponent.tsx
@@ -1,8 +1,26 @@
-import React, {useEffect } from 'react';
-import { MapContainer, TileLayer, Circle, Marker, useMap } from 'react-leaflet';
+import React, {useEffect} from 'react';
+import {Circle, MapContainer, Marker, TileLayer, useMap} from 'react-leaflet';
+import L from 'leaflet';
 import "leaflet/dist/leaflet.css";
 
 import "./Map.css";
+
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+const DefaultIcon = L.icon({
+    iconUrl: markerIcon,
+    iconRetinaUrl: markerIcon2x,
+    shadowUrl: markerShadow,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    tooltipAnchor: [16, -28],
+    shadowSize: [41, 41],
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 interface MapComponentProps {
     coordinates: [number, number] | null;
