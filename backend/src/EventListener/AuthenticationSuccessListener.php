c535fadb8a4a9dcb8936e4b0c44b0d40a3336d12
davy1ex <dezy1ex@gmail.com>
2024-06-20 11:15:16 +0300

    MS4 - AuthenticationSuccessListener.php + routes.yaml + security.yaml + lexik_jwt_authentication.yaml + gesdinet_jwt_refresh_token.yaml - Add backend refresh token

--- backend/src/EventListener/AuthenticationSuccessListener.php
+++ backend/src/EventListener/AuthenticationSuccessListener.php
@@ -1,12 +1,11 @@
 <?php
-
 namespace App\EventListener;
 
 use Gesdinet\JWTRefreshTokenBundle\Model\RefreshTokenManagerInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
 use Symfony\Component\Security\Core\User\UserInterface;
 
-class       AuthenticationSuccessListener
+class AuthenticationSuccessListener
 {
     private $refreshTokenManager;
 
@@ -14,9 +13,7 @@ class       AuthenticationSuccessListener
     {
         $this->refreshTokenManager = $refreshTokenManager;
     }
-    /**
-     * @param AuthenticationSuccessEvent $event
-     */
+
     public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
     {
         $data = $event->getData();
@@ -26,6 +23,16 @@ class       AuthenticationSuccessListener
             return;
         }
 
+        $refreshTokenValue = bin2hex(random_bytes(64));
+
+        $refreshToken = $this->refreshTokenManager->create();
+        $refreshToken->setUsername($user->getUsername());
+        $refreshToken->setRefreshToken($refreshTokenValue);
+        $refreshToken->setValid((new \DateTime())->add(new \DateInterval('P30D')));  // 30 дней
+
+        $this->refreshTokenManager->save($refreshToken);
+
+        $data['refresh_token'] = $refreshToken->getRefreshToken();
         $data['user_data'] = [
             'user_id' => $user->getId(),
             'username' => $user->getUsername(),
