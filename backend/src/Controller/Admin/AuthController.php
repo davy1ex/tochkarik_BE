c1dc84a5df2380f61db2a0f2277aec78c488ebfb
davy1ex <dezy1ex@gmail.com>
2024-06-24 08:54:23 +0300

    MS4-T4 - AuthController.php - Return endpoint to register in admin dashboard

--- backend/src/Controller/Admin/AuthController.php
+++ backend/src/Controller/Admin/AuthController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -26,7 +32,40 @@ class AuthController extends AbstractController
 
         return $this->render('login/index.html.twig', [
             'last_username' => $lastUsername,
-            'error'         => $error,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route('/signup', name: 'appRegisterPage')]
+    public function appRegisterPage(
+        Request                     $request,
+        UserPasswordHasherInterface $userPasswordHasher,
+        Security                    $security,
+        EntityManagerInterface      $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // do anything else you need here, like send an email
+
+            return $security->login($user, 'form_login', 'main');
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form,
         ]);
     }
 
