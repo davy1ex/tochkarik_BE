857d74756498f7a488ba20ed8ace6d03f774eace
davy1ex <dezy1ex@gmail.com>
2024-07-01 14:54:04 +0300

    MS4 - get.nearby.places.ts - Add logic for getting nearby mall/supermarket/restaurant/park

--- frontend/tochkarik/src/components/LocationRulesWidget/hooks/get.nearby.places.ts
+++ frontend/tochkarik/src/components/LocationRulesWidget/hooks/get.nearby.places.ts
@@ -1,12 +1,37 @@
 import axios from "axios";
 
 const getNearbyPlaces = async (latitude: number, longitude: number, radius: number, type: string) => {
+    let typeQuery = '';
+    console.log(type)
+    switch(type) {
+        case 'mall':
+            typeQuery = `node["shop"="mall"](around:${radius},${latitude},${longitude});
+                 way["shop"="mall"](around:${radius},${latitude},${longitude});
+                 relation["shop"="mall"](around:${radius},${latitude},${longitude});`;
+            break;
+        case 'supermarket':
+            typeQuery = `node["shop"="supermarket"](around:${radius},${latitude},${longitude});
+                 way["shop"="supermarket"](around:${radius},${latitude},${longitude});
+                 relation["shop"="supermarket"](around:${radius},${latitude},${longitude});`;
+            break;
+        case 'restaurant':
+            typeQuery = `node["amenity"="restaurant"](around:${radius},${latitude},${longitude});
+                 way["amenity"="restaurant"](around:${radius},${latitude},${longitude});
+                 relation["amenity"="restaurant"](around:${radius},${latitude},${longitude});`;
+            break;
+        case 'park':
+            typeQuery = `node["leisure"="park"](around:${radius},${latitude},${longitude});
+                 way["leisure"="park"](around:${radius},${latitude},${longitude});
+                 relation["leisure"="park"](around:${radius},${latitude},${longitude});`;
+            break;
+        default:
+            throw new Error('Invalid location type');
+    }
+
     const overpassQuery = `
         [out:json];
         (
-            node["shop"="${type}"](around:${radius},${latitude},${longitude});
-            way["shop"="${type}"](around:${radius},${latitude},${longitude});
-            relation["shop"="${type}"](around:${radius},${latitude},${longitude});
+            ${typeQuery}
         );
         out center;
     `;
