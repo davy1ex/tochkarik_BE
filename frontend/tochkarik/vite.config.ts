cf9824500636e3c82d92979421caf37b9b3a8c63
davy1ex <davy1ex@davy1exs-MacBook-Pro.local>
2024-08-31 13:47:07 +0300

    dirty commit

---
+++ frontend/tochkarik/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+
+
+
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env.VITE_API_URL': JSON.stringify(env.SOME_KEY)
+    },
+    plugins: [react()],
+    server: {
+      host: '0.0.0.0',
+      port: 5173
+    }
+  }
+})
